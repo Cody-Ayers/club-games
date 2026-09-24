32f52c41a4eba1613143f7517e3c2d6da2e09e21
Cody-Ayers <122771685+Cody-Ayers@users.noreply.github.com>
2026-09-24 01:08:43 -0600

    Connect players page to shared data

---
+++ src/data/players.ts
@@ -0,0 +1,38 @@
+export const players = [
+    {
+        id: 1,
+        name: "Mike Johnson",
+        ghin: "1234567",
+        handicapIndex: 5.8,
+        preferredTee: "Blue",
+        homeClub: "Flying Horse",
+    },
+
+    {
+        id: 2,
+        name: "Steve Smith",
+        ghin: "7654321",
+        handicapIndex: 8.2,
+        preferredTee: "Blue",
+        homeClub: "Broadmoor",
+    },
+
+    {
+        id: 3,
+        name: "Tom Wilson",
+        ghin: "1111111",
+        handicapIndex: 11.4,
+        preferredTee: "White",
+        homeClub: "Kissing Camels",
+    },
+
+    {
+        id: 4,
+        name: "Jason Brown",
+        ghin: "2222222",
+        handicapIndex: 14.1,
+        preferredTee: "Gold",
+        homeClub: "Flying Horse",
+    },
+
+];
