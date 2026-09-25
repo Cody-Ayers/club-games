397ef015f83ff7125f4a6e9eb9e660264829a90d
Cody-Ayers <122771685+Cody-Ayers@users.noreply.github.com>
2026-09-24 23:34:31 -0600

    Build Pot Allocation Model

--- src/data/templates.ts
+++ src/data/templates.ts
@@ -2,28 +2,70 @@ export const templates = [
     {
         id: 1,
         name: "4 Man 6/6/6",
+
         players: 20,
         buyIn: 60,
+
+        potAllocation: {
+            firstSix: 10,
+            secondSix: 10,
+            thirdSix: 10,
+            grossSkins: 10,
+            netSkins: 10,
+            deuces: 5,
+            netDeuces: 5,
+        }
     },
 
     {
         id: 2,
         name: "2 Man F/B/T",
+
         players: 12,
         buyIn: 60,
+
+        potAllocation: {
+            frontNine: 10,
+            backNine: 10,
+            total: 10,
+            grossSkins: 10,
+            netSkins: 10,
+            deuces: 5,
+            netDeuces: 5,
+        }
     },
 
     {
         id: 3,
         name: "Singles Net Game",
+
         players: 14,
         buyIn: 60,
+
+        potAllocation: {
+            frontNine: 10,
+            backNine: 10,
+            total: 10,
+            grossSkins: 10,
+            netSkins: 10,
+            deuces: 5,
+            netDeuces: 5,
+        }
     },
 
     {
         id: 4,
         name: "Member Guest",
+
         players: 16,
         buyIn: 100,
+
+        potAllocation: {
+            teamGame: 60,
+            grossSkins: 10,
+            netSkins: 10,
+            deuces: 10,
+            netDeuces: 10,
+        }
     },
 ];
